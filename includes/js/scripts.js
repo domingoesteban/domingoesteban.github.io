4948194330c648120cbdd63b45bf2aa30bc70c66
Domingo Esteban <mingo.esteban@gmail.com>
2014-09-25 22:56:04 +0200

    Navbar updated

--- includes/js/scripts.js
+++ includes/js/scripts.js
@@ -45,14 +45,14 @@ $('[rel="popover"]').popover({
 
 jQuery(function () {
 
-    $(window).resize(function () {
+/*    $(window).resize(function () {
         if ($(window).height() > 600) {
             var sH = $(window).height();
         } else {
             var sH = 600;
         }
         $('.header-10-sub').css('height', sH + 'px');
-    });
+    });*/
 
     $('.show_more').click(function(){
       $(this).next('div.hidden-box').slideDown(800);
@@ -60,7 +60,7 @@ jQuery(function () {
        return false;
     });
 
-    $(".links a, .nav a").click(function (event) {
+/*    $(".links a, .nav a").click(function (event) {
         event.preventDefault();
         var dest = 0;
         if ($(this.hash).offset().top > $(document).height() - $(window).height()) {
@@ -69,6 +69,6 @@ jQuery(function () {
             dest = $(this.hash).offset().top;
         }
         $('html,body').animate({scrollTop: dest}, 800, 'swing');
-    });
+    });*/
 
 });
