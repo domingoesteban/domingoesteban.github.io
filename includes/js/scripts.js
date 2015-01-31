3c50a2934271599b105c1b778ab0e2f1bbae542d
Domingo Esteban <mingo.esteban@gmail.com>
2015-01-31 14:35:12 +0100

    Changint robotics page

--- includes/js/scripts.js
+++ includes/js/scripts.js
@@ -96,3 +96,36 @@ $('body').on('activate.bs.scrollspy', function () {
   if(!clicked)$('#mycontent > div > h2').css('padding-top',0);
   clicked = false;
 })
+
+ 
+$(function(){
+  $(document).on( 'scroll', function(){
+   
+  if ($(window).scrollTop() > 100) {
+    $('.scroll-top-wrapper').addClass('show');
+    } else {
+    $('.scroll-top-wrapper').removeClass('show');
+    }
+  });
+});
+
+$(function(){
+  $(document).on( 'scroll', function(){
+   
+  if ($(window).scrollTop() > 100) {
+    $('.scroll-top-wrapper').addClass('show');
+  } else {
+    $('.scroll-top-wrapper').removeClass('show');
+  }
+  });
+   
+  $('.scroll-top-wrapper').on('click', scrollToTop);
+});
+ 
+function scrollToTop() {
+  verticalOffset = typeof(verticalOffset) != 'undefined' ? verticalOffset : 0;
+  element = $('body');
+  offset = element.offset();
+  offsetTop = offset.top;
+  $('html, body').animate({scrollTop: offsetTop}, 500, 'linear');
+}
