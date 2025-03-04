867e829f2a516bdcf923659d91e729f73002045b
Domingo <desteban@anybotics.com>
2025-03-04 01:57:17 +0100

    Add google tag

--- src/consts.ts
+++ src/consts.ts
@@ -3,3 +3,5 @@
 
 export const SITE_TITLE = 'Domingo Esteban';
 export const SITE_DESCRIPTION = 'Personal website of Domingo Esteban';
+export const SITE_KEYWORDS = 'Domingo, Esteban, Robotics, Machine, Learning, Reinforcement, Reinforcement Learning, Deep, Deep Reinforcement Learning, Peru, Switzerland, AI, Artificial, Intelligence, Artificial Intelligence';
+export const GA_TRACKING_ID = 'G-PDKSN9R5M4';
